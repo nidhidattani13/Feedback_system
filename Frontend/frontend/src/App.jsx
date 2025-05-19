bed94b7418236ef91eae5b2c2aaca02cc5e38b4f
nidhidattani13 <dattaninidhi37@gmail.com>
2025-05-19 23:24:10 +0530

    l

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -4,12 +4,12 @@ import Home from "./Pages/Home.jsx";
 import StudentDashboard from "./Pages/Studentdashboard.jsx";
 import FacultyDashboard from "./Pages/FacultyDashboard.jsx";
 import AdminDashboard from "./Pages/AdminDashboard.jsx";
-// import ReportView from "./Pages/ReportView.jsx";
 import Login from "./Pages/Login.jsx";
 import Signup from "./Pages/Signup.jsx";
 import Layout from "./components/Layout.jsx"; // Move Layout into its own file
 import "./styles/Global.css"; // This should import Tailwind
 import "./styles/Theme.css"; // Your custom theme
+import ReportView from "./Pages/Reportview.jsx";
 
 export default function App() {
   return (
@@ -26,7 +26,7 @@ export default function App() {
           <Route path="/feedback-system/student-dashboard" element={<StudentDashboard />} />
           <Route path="/feedback-system/faculty-dashboard" element={<FacultyDashboard />} />
           <Route path="/feedback-system/admin-dashboard" element={<AdminDashboard />} />
-          {/* <Route path="/feedback-system/report" element={<ReportView />} /> */}
+          <Route path="/feedback-system/report" element={<ReportView />} />
         </Route>
       </Routes>
     </Router>
